d739675512473a8d3f108d61332ca2b504228bc0
Phil Cilli <42479528+pcilli@users.noreply.github.com>
2021-02-07 04:00:40 +0000

    Update cpdem_Deriv.m

--- Functions/Subfunctions/cpdem_Deriv.m
+++ Functions/Subfunctions/cpdem_Deriv.m
@@ -1,4 +1,4 @@
-function yprime=cpdem_Deriv(t,y)
+function yprime=cpdem_Deriv(t,y,k2,mu2,sig2,epar)
 % Derivative used in cpdem_Forward
 %
 % The derivative terms in equations 18 & 19, or 20 & 21 of Cilli and
@@ -9,7 +9,7 @@ function yprime=cpdem_Deriv(t,y)
 % properties of rocks using cross-property DEM. Geophysical Journal
 % International, DOI:10.1093/gji/ggab046
 %
-% Berryman, J. G. (1980). Long?wavelength propagation in composite elastic
+% Berryman, J. G. (1980). Long-wavelength propagation in composite elastic
 % media II. Ellipsoidal inclusions. The Journal of the Acoustical Society
 % of America, 68(6), 1820-1831, DOI:10.1121/1.385172
 %
@@ -17,15 +17,7 @@ function yprime=cpdem_Deriv(t,y)
 % physics and phase relations: A handbook of physical constants, 3,
 % 205-228. DOI:10.1029/RF003
 % 
-% Written by Phil Cilli, January 2021 as a part of Cross-Property DEM
-% Toolbox Version 1.0
-
-global inVar;
-
-k2   = inVar(1); 
-mu2  = inVar(2);
-sig2 = inVar(3);
-epar = inVar(4);
+% Written by Phil Cilli, January 2021 as a part of CP_DEM Version 1.0
 
 k0  = y(1);
 mu0 = y(2);
@@ -35,7 +27,7 @@ q  = Q(k0,mu0,k2,mu2,epar); %From Berryman (1980)
 mb = mbar(t,sig2,epar);     %From Cilli and Chapman (2021)
 
 yprime(1,1) = (k2-k0)*p/((sig2-t)*mb);
-yprime(2,2) = (mu2-mu0)*q/((sig2-t)*mb);
+yprime(2,1) = (mu2-mu0)*q/((sig2-t)*mb);
 
 % To do the same calculation using R from Berryman (1995) instead of m-bar:
 % 
