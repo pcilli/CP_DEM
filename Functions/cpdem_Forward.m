814629d48b95c785fe6c57e7ce52470849998710
Phil Cilli <42479528+pcilli@users.noreply.github.com>
2021-02-07 03:58:27 +0000

    Update cpdem_Forward.m

--- Functions/cpdem_Forward.m
+++ Functions/cpdem_Forward.m
@@ -12,18 +12,10 @@ function [k0, mu0] = cpdem_Forward(k1,mu1,sig1,k2,mu2,sig2,sig0,epar)
 % Written by Phil Cilli, January 2021 as a part of Cross-Property DEM
 % Toolbox Version 1.0
 
-global inVar;
+%Optimisation routine only tested on MATLAB R2019b.
+[T,Y] = ode45(@(t,y)cpdem_Deriv(t,y,k2,mu2,sig2,epar),[sig1; sig0],[k1; mu1],1e-5);
 
-inVar = zeros(4,1);
-inVar(1) = k2; 
-inVar(2) = mu2;
-inVar(3) = sig2;
-inVar(4) = epar;
-
-%Optimisation routine only tested on MATLAB R2019B.
-[tout,yout] = ode45('cpdem_Deriv',[sig1; sig0],[k1; mu1],1e-5);
-
-k0  = yout(length(tout),1);
-mu0 = yout(length(tout),2);
+k0  = Y(length(T),1);
+mu0 = Y(length(T),2);
 
 end
